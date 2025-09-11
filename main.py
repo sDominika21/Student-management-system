d71bc7c9a0e802f540bb8b4a764203273d89e2f4
doma <sochadominika123@gmail.com>
2025-09-11 15:17:30 +0200

    Create an About Dialog

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class MainWindow(QMainWindow):
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
+        about_action.triggered.connect(self.about)
 
         search_action = QAction(QIcon("icons/search.png"), "Search", self)
         search_action.triggered.connect(self.search)
@@ -88,6 +89,10 @@ class MainWindow(QMainWindow):
 
     def delete(self):
         dialog = DeleteDialog()
+        dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
         dialog.exec()
 
 
@@ -272,6 +277,15 @@ class DeleteDialog(QDialog):
         confirmation_widget.setText("The record was deleted successfully!")
         confirmation_widget.exec()
 
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        Hello There! 
+        """
+        self.setText(content)
 
 
 app = QApplication(sys.argv)
